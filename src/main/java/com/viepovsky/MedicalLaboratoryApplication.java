f0a42eeeb82e31aeeb993eda78bc669e06380d1d
Oskar R <o.rajzner@gmail.com>
2023-04-13 17:43:45 +0200

    Medical Laboratory Service initial commit

---
+++ src/main/java/com/viepovsky/MedicalLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.viepovsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalLaboratoryApplication.class, args);
+	}
+
+}

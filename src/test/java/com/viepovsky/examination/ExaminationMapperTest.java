3b9ee352c02357cea01b33f7c3205087d4c60e7a
Oskar R <o.rajzner@gmail.com>
2023-04-21 00:11:25 +0200

    Added test unit class for examination mapper

---
+++ src/test/java/com/viepovsky/examination/ExaminationMapperTest.java
@@ -0,0 +1,55 @@
+package com.viepovsky.examination;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ExaminationMapperTest {
+    private final ExaminationMapper mapper = new ExaminationMapper();
+
+    @Test
+    void should_map_Examination_to_ExaminationDTO() {
+        //Given
+        var examination = Examination.builder().id(5L).name("Test").type(ExaminationType.BLOOD).cost(BigDecimal.valueOf(50)).build();
+        //When
+        var mappedExamination = mapper.mapToExaminationDto(examination);
+        //Then
+        assertThat(mappedExamination).isNotNull();
+        assertEquals(examination.getId(), mappedExamination.getId());
+        assertEquals(examination.getType(), mappedExamination.getType());
+        assertEquals(examination.getCost(), mappedExamination.getCost());
+    }
+
+    @Test
+    void should_map_Examination_list_to_ExaminationDTO_list() {
+        //Given
+        var examination = Examination.builder().id(5L).name("Test").type(ExaminationType.BLOOD).cost(BigDecimal.valueOf(50)).build();
+        List<Examination> examinations = new ArrayList<>(List.of(examination));
+        //When
+        List<ExaminationDTO> mappedExaminations= mapper.mapToExaminationDtoList(examinations);
+        //Then
+        assertThat(mappedExaminations).isNotNull();
+        assertEquals(examinations.size(), mappedExaminations.size());
+    }
+
+    @Test
+    void should_map_ExaminationDTO_to_Examination() {
+        //Given
+        var examination = ExaminationDTO.builder().id(5L).name("Test").type(ExaminationType.BLOOD).cost(BigDecimal.valueOf(50)).build();
+        //When
+        var mappedExamination = mapper.mapToExamination(examination);
+        //Then
+        assertThat(mappedExamination).isNotNull();
+        assertEquals(examination.getName(), mappedExamination.getName());
+        assertEquals(examination.getType(), mappedExamination.getType());
+        assertEquals(examination.getCost(), mappedExamination.getCost());
+    }
+}

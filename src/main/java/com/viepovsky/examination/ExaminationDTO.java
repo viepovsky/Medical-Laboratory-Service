798119bb7c5a233cfb134a258ecc1d95b84f292c
Oskar R <o.rajzner@gmail.com>
2023-04-20 22:20:59 +0200

    Added implementation of Examination DTO and Mapper

--- src/main/java/com/viepovsky/examination/ExaminationDTO.java
+++ src/main/java/com/viepovsky/examination/ExaminationDTO.java
@@ -1,4 +1,32 @@
 package com.viepovsky.examination;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class ExaminationDTO {
+    private Long id;
+
+    @NotBlank(message = "Name must not be empty")
+    private String name;
+
+    @NotNull(message = "Type must not be null")
+    private ExaminationType type;
+
+    @NotBlank(message = "Short description must not be empty")
+    private String shortDescription;
+
+    private String longDescription;
+
+    @NotNull(message = "Cost must not be null")
+    private BigDecimal cost;
 }
--- src/main/java/com/viepovsky/examination/ExaminationMapper.java
+++ src/main/java/com/viepovsky/examination/ExaminationMapper.java
@@ -1,4 +1,35 @@
 package com.viepovsky.examination;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 class ExaminationMapper {
+    ExaminationDTO mapToExaminationDto(Examination examination) {
+        return new ExaminationDTO(
+                examination.getId(),
+                examination.getName(),
+                examination.getType(),
+                examination.getShortDescription(),
+                examination.getLongDescription(),
+                examination.getCost()
+        );
+    }
+
+    List<ExaminationDTO> mapToExaminationDtoList(List<Examination> examinations) {
+        return examinations.stream()
+                .map(this::mapToExaminationDto)
+                .toList();
+    }
+
+    Examination mapToExamination(ExaminationDTO examinationDTO) {
+        return Examination.builder()
+                .name(examinationDTO.getName())
+                .type(examinationDTO.getType())
+                .shortDescription(examinationDTO.getShortDescription())
+                .longDescription(examinationDTO.getLongDescription())
+                .cost(examinationDTO.getCost())
+                .build();
+    }
 }

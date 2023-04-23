d00fec3ec4249a42381536678104e7e489044f5c
Oskar R <o.rajzner@gmail.com>
2023-04-24 01:11:13 +0200

    Changes in JwtService class, added methods
    Added methods for: generating jwt, validating jwt, extracting claims

--- src/main/java/com/viepovsky/security/JwtService.java
+++ src/main/java/com/viepovsky/security/JwtService.java
@@ -2,29 +2,65 @@ package com.viepovsky.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 class JwtService {
     private final static String SECRET_KEY = "566B59703373367639792442264528482B4D6251655468576D5A713474377721";
-    String extractUserLogin(String token) {
-        return extractClaim(token, Claims::getSubject);
+
+    String extractUserLogin(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
     }
-    <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
+
+    <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
         return claimsResolver.apply(claims);
     }
-    private Claims extractAllClaims(String token) {
+
+    String generateJwtToken(UserDetails userDetails) {
+        return generateJwtToken(new HashMap<>(), userDetails);
+    }
+    String generateJwtToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String userLogin = extractUserLogin(jwtToken);
+        return userLogin.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken);
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+
+    private Claims extractAllClaims(String jwtToken) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJwt(jwtToken)
                 .getBody();
     }
 

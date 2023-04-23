bf45bd379b6e6774da393e3d93c5ede3135c6b14
Oskar R <o.rajzner@gmail.com>
2023-04-24 00:41:47 +0200

    Added methods for extracting user login from jwt
    Implementation made in JwtService class

--- src/main/java/com/viepovsky/security/JwtService.java
+++ src/main/java/com/viepovsky/security/JwtService.java
@@ -1,10 +1,35 @@
 package com.viepovsky.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 class JwtService {
+    private final static String SECRET_KEY = "566B59703373367639792442264528482B4D6251655468576D5A713474377721";
     String extractUserLogin(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

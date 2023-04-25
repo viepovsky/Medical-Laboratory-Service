f54fae6e7951acfac7fea0027da34b59ed2a6721
Oskar R <o.rajzner@gmail.com>
2023-04-25 12:31:01 +0200

    Created application-test.yml, changed UserController unit class tests

--- src/test/java/com/viepovsky/user/UserControllerTest.java
+++ src/test/java/com/viepovsky/user/UserControllerTest.java
@@ -1,24 +1,45 @@
 package com.viepovsky.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.viepovsky.exceptions.PasswordValidationException;
+import com.viepovsky.user.dto.request.RegisterUserRequest;
+import com.viepovsky.user.dto.request.UpdateUserRequest;
+import com.viepovsky.user.dto.response.CreatedUserResponse;
+import com.viepovsky.user.dto.response.UserDetailsResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class UserControllerTest {
     @Autowired
@@ -30,88 +51,184 @@ class UserControllerTest {
     @MockBean
     private UserMapper mapper;
 
-    @Test
-    void should_get_user_for_login_purposes() throws Exception {
-        //Given
-        var user = new UserDTO("testLogin", "testPassword", Role.USER);
-        var json = new ObjectMapper().writeValueAsString(user);
+    @MockBean
+    private UserDetailsService userDetailsService;
 
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    public static String generateToken(String username, String secretKey) {
+        return Jwts
+                .builder()
+                .setClaims(new HashMap<>())
+                .setSubject(username)
+                .setIssuer("medical-app.com")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignInKey(secretKey), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    private static Key getSignInKey(String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Test
+    void should_get_user_details() throws Exception {
+        //Given
+        var userInDb = User.builder().login("testLogin").role(Role.ROLE_USER).build();
+        var userResponse = UserDetailsResponse.builder().login("testLogin").firstName("testName").build();
+        var jsonResponse = new ObjectMapper().writeValueAsString(userResponse);
+        var jwtToken = generateToken("testLogin", secretKey);
+
+        when(userDetailsService.loadUserByUsername("testLogin")).thenReturn(userInDb);
         when(service.getUserByLogin(anyString())).thenReturn(Mockito.mock(User.class));
-        when(mapper.mapToUserDtoForLogin(any(User.class))).thenReturn(user);
+        when(mapper.mapToUserDetailsResponse(any(User.class))).thenReturn(userResponse);
         //When & then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/medical/users")
-                        .param("login", "someString"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(json));
+                        .param("login", "testLogin")
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+    }
+
+    @Test
+    void should_not_get_user_details_if_token_login_doesnt_match_with_given_login() throws Exception {
+        //Given
+        var userInDb = User.builder().login("testLogin22").role(Role.ROLE_USER).build();
+        var jwtToken = generateToken("testLogin", secretKey);
+
+        when(userDetailsService.loadUserByUsername("testLogin")).thenReturn(userInDb);
+        //When & then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/medical/users")
+                        .param("login", "testLogin")
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     void should_create_user() throws Exception {
         //Given
-        var userDTO = UserDTO.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
-                .email("email@email.com").name("test").lastName("testlast").build();
-        var json = new ObjectMapper().writeValueAsString(userDTO);
-        var user = User.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
-                .email("email@email.com").name("test").lastName("testlast").build();
+        var adminInDb = User.builder().login("testAdmin").role(Role.ROLE_ADMIN).build();
+        var jwtToken = generateToken("testAdmin", secretKey);
+        var userRequest = RegisterUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
+                .email("email@email.com").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
+        var userResponse = CreatedUserResponse.builder().login("testLogin").id(5L).build();
+        var jsonResponse = new ObjectMapper().writeValueAsString(userResponse);
 
-        when(mapper.mapToUser(any(UserDTO.class))).thenReturn(user);
-        when(service.createUser(any(User.class))).thenReturn(user);
-        when(mapper.mapToCreatedUserDto(any(User.class))).thenReturn(userDTO);
+        when(userDetailsService.loadUserByUsername("testAdmin")).thenReturn(adminInDb);
+        when(mapper.mapToUser(any(RegisterUserRequest.class))).thenReturn(Mockito.mock(User.class));
+        when(service.createUser(any(User.class))).thenReturn(Mockito.mock(User.class));
+        when(mapper.mapToCreatedUserResponse(any(User.class))).thenReturn(userResponse);
         //When & then
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/medical/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.content().json(json))
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse))
                 .andExpect(MockMvcResultMatchers.header().string("Location", "/medical/users?login=testLogin"));
     }
 
     @Test
     void should_not_create_user_with_invalid_body_values() throws Exception {
         //Given
-        var userDTO = UserDTO.builder().login("testLogin").personalId("9412").password("testPassword22@")
-                .email("email").name("test").lastName("testlast").build();
-        var json = new ObjectMapper().writeValueAsString(userDTO);
+        var adminInDb = User.builder().login("testAdmin").role(Role.ROLE_ADMIN).build();
+        var jwtToken = generateToken("testAdmin", secretKey);
+        var userRequest = RegisterUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword")
+                .email("email").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
+
+        when(userDetailsService.loadUserByUsername("testAdmin")).thenReturn(adminInDb);
         //When & then
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/medical/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void should_update_user() throws Exception {
+    void should_not_create_user_if_token_is_from_user_with_UserRole() throws Exception {
         //Given
-        var userDTO = UserDTO.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
-                .email("email@email.com").name("test").lastName("testlast").build();
-        var json = new ObjectMapper().writeValueAsString(userDTO);
-        var user = User.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
-                .email("email@email.com").name("test").lastName("testlast").build();
+        var userInDb = User.builder().login("testUser").role(Role.ROLE_USER).build();
+        var jwtToken = generateToken("testUser", secretKey);
+        var userRequest = RegisterUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
+                .email("email@email.com").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
 
-        when(mapper.mapToUser(any(UserDTO.class))).thenReturn(user);
-        doNothing().when(service).updateUser(any(User.class));
+        when(userDetailsService.loadUserByUsername("testUser")).thenReturn(userInDb);
+        //When & then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/medical/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void should_update_user() throws Exception, PasswordValidationException {
+        //Given
+        var user = User.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
+                .email("email@email.com").name("test").lastName("testlast").role(Role.ROLE_USER).build();
+        var jwtToken = generateToken("testLogin", secretKey);
+        var userRequest = UpdateUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
+                .email("email@email.com").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
+
+        when(userDetailsService.loadUserByUsername(anyString())).thenReturn(user);
+        when(mapper.mapToUser(any(UpdateUserRequest.class))).thenReturn(user);
+        doNothing().when(service).updateUserWithPassword(any(User.class));
         //When & then
         mockMvc.perform(MockMvcRequestBuilders
                         .put("/medical/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void should_not_update_user_if_token_login_doesnt_match_with_given_login() throws Exception {
+        //Given
+        var userInDb = User.builder().login("testLogin22").role(Role.ROLE_USER).build();
+        var jwtToken = generateToken("testLogin", secretKey);
+        var userRequest = UpdateUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword22@")
+                .email("email@email.com").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
+
+        when(userDetailsService.loadUserByUsername(anyString())).thenReturn(userInDb);
+        //When & then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/medical/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     void should_not_update_user_with_invalid_body_values() throws Exception {
         //Given
-        var userDTO = UserDTO.builder().login("testLogin").personalId("9412").password("testPassword22@")
-                .email("email").name("test").lastName("testlast").build();
-        var json = new ObjectMapper().writeValueAsString(userDTO);
+        var userInDb = User.builder().login("testLogin").role(Role.ROLE_USER).build();
+        var jwtToken = generateToken("testLogin", secretKey);
+        var userRequest = UpdateUserRequest.builder().login("testLogin").personalId("98062267819").password("testPassword")
+                .email("email").firstName("test").lastName("testlast").build();
+        var jsonRequest = new ObjectMapper().writeValueAsString(userRequest);
+
+        when(userDetailsService.loadUserByUsername(anyString())).thenReturn(userInDb);
         //When & then
         mockMvc.perform(MockMvcRequestBuilders
                         .put("/medical/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                        .content(jsonRequest)
+                        .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isBadRequest());
     }
 }

50c3a75b42486ed4a3c77602ee851edbccfec35f
Oskar R <o.rajzner@gmail.com>
2023-12-07 10:47:47 +0100

    Fix wrong flyway file names

--- src/main/java/db/migration/V3__insert_to_users_table.java
+++ src/main/java/db/migration/V3__insert_to_users_table.java
@@ -5,14 +5,12 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
-public class V8__insert_testadmin_to_users_and_update_testuser extends BaseJavaMigration {
+public class V3__insert_to_users_table extends BaseJavaMigration {
     public void migrate(Context context) {
         new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true))
                 .execute("INSERT INTO USERS(id, login, pesel, password, email, name, last_name, phone_number, role) " +
-                        "VALUES (2, 'testadmin', '98062267817', '$2a$12$w4YkVfMkwnsS7qAwo18ajuNAi6T.W6mxQQPHVAY6hFMlp.egK0V5m'," +
-                        "'test@email.com', 'Testname', 'TestLastName', '777777777', 'ROLE_ADMIN')");
+                        "VALUES (1, 'testuser', '98062267819', '$2a$12$w4YkVfMkwnsS7qAwo18ajuNAi6T.W6mxQQPHVAY6hFMlp.egK0V5m'," +
+                        "'test@email.com', 'Testname', 'TestLastName', '777777777', 0)");
 
-        new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true))
-                .execute("UPDATE users SET role = 'ROLE_USER' WHERE role = 'USER';");
     }
 }
